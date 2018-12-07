4801f4c70cf8b01496bdf0e4ac78335405bedd34
hamza-hd <hamza16haddad@gmail.com>
2018-12-07 16:30:36 +0100

    first commit

---
+++ src/main/java/com/sid/ServerGestionStockApplication.java
@@ -0,0 +1,12 @@
+package com.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockApplication.class, args);
+	}
+}
